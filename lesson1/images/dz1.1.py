6800a84f80ff56139750d57e575bf70a9b33cee9
artsyomholubev <temg7405@yandex.ru>
2023-05-17 22:00:23 +0300

    “lesson1”

---
+++ lesson1/images/dz1.1.py
@@ -0,0 +1,8 @@
+a, b = int(input('Введите переменную а: ')), int(input('Введите переменную b: '))
+print('')
+print('Сумма a+b:', a + b, end='\n')
+print('Разность a-b:', a - b, end='\n')
+print('Произведение a*b:', a * b, end='\n')
+print('а в степени b:', a ** b, end='\n')
+print('Остаток деления a на b:', a % b, end='\n')
+print('Целая часть от деления a на b:', a // b, end='\n')

25cb9bdb8ac5ab01ec74ca3a25a53ce1bc88cfcb
ArtyomHolubev <wtftem206@gmail.com>
2023-07-04 19:11:37 +0300

    homework 12

--- lesson12/homework12.2.py
+++ lesson12/homework12.2.py
@@ -5,18 +5,15 @@ class RandomValue:
     def __init__(self, limit, *args):
         self._some_list = list(args)
         self._limit = limit
-        self._curr_index = 0
         for element in range(0, self._limit):
             self._some_list.append(random.randint(1, 100))
-
 
     def __str__(self):
         return f"MyList object: {self._some_list}"
 
     def __iter__(self):
-        if self._curr_index < self._limit:
-            for element in self._some_list:
-                yield element
+        for element in self._some_list:
+            yield element
         return print("iterator is stopped")
 
 

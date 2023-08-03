ffe79e68468b4f06532f774e48131324b1a539d0
ArtyomHolubev <wtftem206@gmail.com>
2023-08-03 19:35:36 +0300

    toml add

--- lesson21_client_server_database/db/config.py
+++ lesson21_client_server_database/db/config.py
@@ -1,4 +1,4 @@
-db_user = "admin"
+db_user = "postgres"
 db_pass = "postgres"
 db_host = "127.0.0.1"
 db_port = 5432
--- lesson21_client_server_database/play.py
+++ lesson21_client_server_database/play.py
@@ -15,11 +15,9 @@ async def main():
         age=34,
     )
     post_tile, post_description = f"{user.name} new post", "I love you!"
-    # post_tile, post_description = f"Alex 1st post", "Hello! I am Alex, I am 34 years old "
     client = Client(server_url=SERVER_URL)
-    # add_post_result = await client.add_post(user, post_tile,
-    # post_description)Alex 1st post', 'Hello! I am Alex, I am 34 years old
-    # print(add_post_result)
+    add_post_result = await client.add_post(user, post_tile, post_description)
+    print(add_post_result)
     #
     # add_comment_result = await client.add_comment(user, post_tile, post_description, comment_title="Great Post!")
     # print(add_comment_result)
@@ -30,14 +28,14 @@ async def main():
     # delete_like_result = await client.delete_like(user, 'Alex 2nd post',
     # 'My favourite programming language is Python!')
     # print(delete_like_result)
-
+    #
     # user_with_comment = User(name="Ann", age=32)
     # delete_comment_result = await client.delete_comment(user_with_comment, "Ann 1st post",
     # "Hello! I am Ann, I am 32 years old", "I am 32 years old too!")
     # print(delete_comment_result)
-
-    delete_post_result = await client.delete_post(user, "Alex 1st post", "Hello! I am Alex, I am 34 years old")
-    print(delete_post_result)
+    #
+    # delete_post_result = await client.delete_post(user, "Alex 1st post", "Hello! I am Alex, I am 34 years old")
+    # print(delete_post_result)
 
 
 if __name__ == '__main__':
